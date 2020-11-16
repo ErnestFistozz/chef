9598c38ba12c968115a413fbdcc9d9a92e1412a1
Ernest Mamba <ErnestM@Nedbank.co.za>
2020-11-17 00:55:36 +0200

    added AWS books text file

---
+++ AWS_Books.md
@@ -0,0 +1,43 @@
+## AWS Books to read for beginners
+
+
+AWS Books
+
+Learning AWS
+Aws: The Ultimate Guide From Beginners To Advanced For The Amazon Web Services (2020 Edition) Theo H. King
+
+Aws: AMAZON WEB SERVICES: The Complete Guide From Beginners For Amazon Web Services
+
+Aws: Amazon Web Services Tutorial for Beginners
+
+AWS Basics: Beginners Guide
+
+Aws: Amazon Web Services. A Complete Guide from Beginners to Advanced. Steve Fedler
+
+Aws: Amazon Web Services, the Ultimate Guide for Beginners to Advanced Maverick Koston
+
+Aws: START AMAZON WEB SERVICES Learn Amazon Web Services from the Scratch and Become a Cloud Guru Cloud Gurus
+
+Aws: The Complete Guide From Beginners To Advanced For Amazon Web Services Julian Gramm
+
+Introduction to AWS (Amazon Web Services) Beginner's Guide Book: Learning the Basics of AWS in an Easy and Fast Way G. Sammons
+
+Learning Amazon Web Services (AWS) A Hands-On Guide to the Fundamentals of AWS Cloud
+
+
+MONTH OF LUNCHES
+https://b-ok.africa/s/Learn%20Amazon%20Web%20Services%20in%20a%20Month%20of%20Lunches
+
+Learn Amazon Web Services in a Month of Lunches
+
+
+
+### Random Books
+
+https://b-ok.africa/book/5419050/124f66 - SQL: The Ultimate Guide From Beginner To Expert - Learn And Master SQL In No Time! (2017 Edition)
+
+https://b-ok.africa/book/5854218/42c865 - Python Programming: A Step By Step Guide from Beginner to Advanced (Beginner & Advanced)
+
+https://b-ok.africa/book/5850678/c3e52d - Python Crash Course: The Ultimate Step-By-Step Guide to Learn, Understand, and Master Python Programming and Computer Coding Language (From Beginners to Advanced)
+
+https://b-ok.africa/book/2544221/452607 - Learn HTML - From Beginner To Advanced: HTML Complete Guide
--- learn_chef_ii.rb
+++ learn_chef_ii.rb
@@ -1,3 +1,5 @@
+'bootstrap a windows machine'
+
 #Ohai
 'is a tool that captures details/attributes about a node'
 
@@ -51,6 +53,138 @@ String interpolation only works in ruby files
 	
 				<%= node['ipaddress'] %>
 				<%= node['hostname'] %>
+
+
+'Checking the client'
+knife client list
+
+ORGNAME-validator ---> should be returned
+	'This is a special key that has access to the Chef Server.'
+
+have a .chef directory, within the chef repository, which contains the knife configuration file (knife.rb),
+ 'personal key', and 'organizational key'
+
+#Upload a cookbook to chef server
+'To upload a cookbook to the Chef Server you need to be within the directory of the cookbook.'
+
+#Berkshelf
+'Berkshelf' ---> is a cookbook management tool that allows us to upload your cookbooks and all of its dependencies to the Chef Server.
+
+Berksfile.lock ---> is a receipt of all the cookbooks and dependencies found at the exact moment that you ran 'berks install'
+
+A node can only join one organization
+
+To be a node means that it has Chef installed, has configuration files in place, and when you run the chef-client
+application with no parameters it will successfullycontact the Chef Server and ask it for the run list that it 
+should apply and the cookbooks required to execute that run list
+
+BOOTSTRAPPING A NODE
+'knife bootstrap IP_Address -U Username -P Password --SUDO -N NODE_NAME' ---> Applies to a Unix based machine/node
+'knife bootstrap windows winrm IP_Address -x Administrator -P super_secret_password' ---> for bootstraping a windows machine/node
+Note:: Port 
+
+
+Adding a cookbook to a runlist
+
+'knife node run_list add NODE_NAME COOKBOOK_NAME'
+#Example
+knife node run_list add xdvmamba1 learn_chef_httpd
+		#OR
+knife node run_list add xdvmamba1 "recipe[COOKBOOK_NAME::RECIPE_NAME]"
+
+Removing a cookbook to a run_list
+
+'knife node run_list removed NODE_NAME "recipe[COOKBOOK_NAME::RECIPE_NAME]" '
+
+
+#Attributes
+'Attributes defined in a cookbook are not considered automatic. They are simply default values that we may change.'
+
+#Wrapper Cookbook
+'wrapper cookbook is a new cookbook that encapsulates the functionality of the original cookbook.'
+It defines new default values for the recipes.
+
+'This is a common method for overriding cookbooks because it allows us to leave the original cookbook untouched. 
+We simply provide new default values that we want and then include the recipes that we want to run.'
+
+#Naming Cookbooks and Wrapper Cookbooks
+Traditionally we would name the cookbook with a prefix of the name of our company and then follow it by the cookbook name 'company-cookbook'.
+Example: Nedbank-IIS Or Nedbank-Wrapper-Haproxy or Nedbank-Postilion-Patch
+
+#For a cookbook to be a a wrapper cookbook it has to include another cookbook as a dependency and override its 
+#Attributes while preserving its functionality
+depends 'COOKBOOK_NAME', '~> VERSION' --> version must correspond with a version with exist
+
+#The default of the wrapper cookbook has to include the recipes required to be run
+#Typically includes the default cookbook
+
+include_recipe 'COOKBOOK_NAME::RECIPE_NAME'
+#Example 
+include_recipe 'COOKBOOK_NAME::default'
+
+#To override values in the original cookbook, new defaults need to be put in the default recipe of the 
+# wrapper cookbook, before including the recipe
+
+'
+	node['COOKBOOK_NAME']['ATTRIBUTE_NAME'] = NEW_VALUES #--->Overriding values
+	include_recipe 'COOKBOOK_NAME::default'
+
+'
+
+To have the dependency cookbook included
+
+Inside the cookbook execute/run 'berks install' ---> Ensure  Berksfile is there
+upload cookbook to the chef server 'berks upload' ---> Ensure Berksfile is there
+
+#Running commands againts multiple commands
+
+knife ssh ---> 'ssh that allows us to execute a command across multiple nodes that match a specified search query.'
+
+#Run commands accross all nodes that are part of organisation
+knife ssh " *:* " -U Username -P Password "sudo chef-client"
+
+knife node run_list add NODE_NAME "recipe[COOKBOOK_NAME::RECIPE_NAME]"
+
+#ROLES
+
+'role' ---> describes a run list of recipes that are executed on the node. A role may also define new defaults 
+			or overrides for existing cookbook attribute values.
+			When you assign a role to a node you do so in its run list. This allows you to configure many nodes in a similar fashion.
+
+# Defining a ROLE
+This is a ruby file that contains specific methods that allow you to express details about the role. 
+Youll see that the role has a name, a description, and run list.
+
+The name of the role as a practice will share the name of the ruby file unless it cannot for some reason. 
+The name of the role should clearly describe what it attempts accomplish.
+
+
+The run list defines the list of recipes that give the role its purpose. 
+Example ---> Currently the load_balancer role defines a single recipe - the myhaproxy cookbook~s default recipe.
+
+#UPLOADING ROLE TO CHEF SERVER
+
+knife role from file ROLE_FILE_NAME
+
+'To show role details'
+
+knife role show ROLE_NAME
+
+#setting a role to a node
+
+knife node run_list set NODE_NAME "role[ROLE_NAME]"
+
+#The above command replaces the existing runlist
+
+This is a command that allows us to set the run list to a value provided.
+This will replace the existing run list with a new one that we provide.
+
+'For a node existing in the cloud, the actual EXTERNAL IPADDRESS of a node are stored in a variable called::cloud'
+knife node show NODE_NAME -a cloud
+
+'To replace a default attribute in a recipe'
+you use:
+		node.default['COOKBOOK_NAME']['ATTRIBUTE_NAME'] instead of node['COOKBOOK_NAME']['ATTRIBUTE_NAME']
 
 
 
